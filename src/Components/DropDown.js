6b5c00792fe1f1fb0a5a9ce30217ea7864ed16c2
wooyoungkim <whoyoung90@naver.com>
2021-06-10 22:56:39 +0900

    add: 210610 Dropdown 작업완료

--- src/Components/DropDown.js
+++ src/Components/DropDown.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { deliveryTypes } from '../data';
 import styled from 'styled-components';
 
 function DropDown() {
-  return <div>.</div>;
+  return (
+    <SelectBox>
+      <OptionDefault>선택해주세요.</OptionDefault>
+      {deliveryTypes.map((el, idx) => (
+        <Option key={idx} value={el.id}>
+          {el.name} {el.delivery_price}원
+        </Option>
+      ))}
+    </SelectBox>
+  );
 }
 
 export default DropDown;
+
+const SelectBox = styled.select`
+  margin-top: 20px;
+  width: 330px;
+  height: 40px;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+const OptionDefault = styled.option`
+  font-size: 18px;
+  font-weight: bold;
+`;
+const Option = styled.option`
+  font-weight: bold;
+`;
--- src/Pages/Cart.js
+++ src/Pages/Cart.js
@@ -1,13 +1,62 @@
 import React from 'react';
+import DropDown from '../Components/DropDown';
 import styled from 'styled-components';
+import { css } from 'styled-components';
 
 function Cart() {
-  return <Test>초기세팅</Test>;
+  return (
+    <Wrap>
+      <Title>장바구니</Title>
+      <AddressWrap>주소</AddressWrap>
+      <Address>서울시 강남구 도산대로 174 7층</Address>
+      <DeliverWrap>배송 방법</DeliverWrap>
+      <DropDown />
+      <ProductInfo>상품 내역</ProductInfo>
+      <InputCheckbox /> 전체
+    </Wrap>
+  );
 }
 
 export default Cart;
 
-const Test = styled.div`
-  font-size: 14px;
+const Wrap = styled.div``;
+
+const Title = styled.h1`
+  padding-bottom: 50px;
+  border-bottom: 1px solid gray;
   color: black;
+  font-size: 50px;
+  font-weight: bold;
+`;
+
+const Mixin = css`
+  margin-top: 20px;
+  color: black;
+  font-size: 30px;
+  font-weight: bold;
+`;
+
+const AddressWrap = styled.div`
+  ${Mixin}
+`;
+const Address = styled.div`
+  margin-top: 20px;
+  font-size: 20px;
+`;
+
+const DeliverWrap = styled.div`
+  ${Mixin}
+`;
+
+const ProductInfo = styled.div`
+  ${Mixin}
+`;
+
+const InputCheckbox = styled.input.attrs({
+  type: 'checkbox',
+})`
+  margin: 8px;
+  width: 17px;
+  height: 17px;
+  border-bottom: 1px solid gray;
 `;
